d8d2dc4815933e550ad85b0ee6c1e192aa47ad4b
Mastergammler <mg.coffee.man.goa@gmail.com>
2024-09-15 12:50:17 +0200

    Parsing is working, but no way to add the metadata to the dataview object

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, MetadataCache, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, Vault } from 'obsidian';
+import { exec } from 'child_process'
+import { promisify } from 'util'
+import * as path from 'path'
+import * as parser from './parser'
+import { GitFile } from './parser'
 
 // Remember to rename these classes and interfaces!
 
@@ -7,8 +12,10 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default' 
+	mySetting: 'default'
 }
+
+const execAsync = promisify(exec);
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,54 +23,66 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const indexFileName = "index.git-index-metadata";
+		const vaultPath = this.app.vault.adapter.getBasePath();
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'git-index-metadata',
+			name: 'Index git timestamps',
+			callback: async () => {
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				//NOTE: get file function is too stupid to find when it's an extension only
+
+				// write history to file
+				new Notice('Start indexing, this might take a sec');
+				const { stdout, stderr } = await execAsync(`git --no-pager log --date=iso --numstat --reverse > ${indexFileName}`, { cwd: vaultPath });
+				if (stderr) new Notice('Git error: ' + stderr);
+				else new Notice('Indexing to file done');
+
 			}
 		});
+
+		this.addCommand({
+			id: 'git-parse-index',
+			name: 'Apply current git index',
+			callback: async () => {
+
+				const file = this.app.vault.getAbstractFileByPath(indexFileName);
+				const content = await this.app.vault.cachedRead(file);
+				const gitFileMap = parser.parseGithistoryIndex(content);
+
+				console.log(gitFileMap);
+				const allFiles = this.app.vault.getMarkdownFiles();
+				const fileCache = this.app.metadataCache;
+
+
+				const dataview = this.app.plugins.plugins.dataview.api;
+				const allpages = dataview.pages();
+
+				allpages.forEach(f => addToFileMedata(f, fileCache, gitFileMap));
+			}
+		});
+
+		function addToFileMedata(dvPage: any, metadataCache: MetadataCache, metadata: Map<string, GitFile>) {
+
+			const file = dvPage.file;
+
+			if (!metadata.has(file.name)) {
+				//console.log("No metadata found for file: ", file.name)
+				return;
+			}
+
+			console.log("Found metadata for", file.name);
+			console.log(file);
+			const cache = metadataCache.getFileCache(file)
+			console.log(cache)
+			const gitMetadata = metadata.get(file.name);
+
+			if (!file.git) file.git = {}
+
+			Object.assign(file.git, gitMetadata);
+		}
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
@@ -91,22 +110,6 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -116,7 +119,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
---
+++ src/parser.ts
@@ -0,0 +1,55 @@
+
+export interface GitFile {
+	ctime: Date;
+	mtime: Date;
+	history: GitEditHistory[];
+}
+
+export interface GitEditHistory {
+	date: Date;
+	linesAdded: number;
+	linesRemoved: number;
+}
+
+function parseLogEntry(logEntry: string, map: Map<string, GitFile>) {
+
+	const lines = logEntry.split('\n');
+
+	// parse date
+	const dateLine = lines.find(line => line.trim().startsWith('Date:'));
+	const dateStr = dateLine?.substring(dateLine.indexOf(":"));
+	const date = new Date(dateStr);
+
+	const fileLines = lines.filter(l => /^\d/.test(l)).map(l => {
+		// info is spearated by tabs
+		const splits = l.split('\t')
+		return {
+			name: splits[2],
+			date: date,
+			linesAdded: Number(splits[0]),
+			linesRemoved: Number(splits[1])
+		}
+	});
+
+	// parse all lines
+	fileLines.forEach(l => {
+		if (map.has(l.name)) {
+			const entry = map.get(l.name);
+			entry.history.push(l as GitEditHistory);
+			entry.mtime = l.date;
+		}
+		else {
+			map.set(l.name, { ctime: l.date, mtime: l.date, history: [l as GitEditHistory] });
+		}
+	});
+}
+
+export function parseGithistoryIndex(content: string): Map<string, GitFile> {
+	// splitting at author beacuse lower risk of catching a file name
+	// - so first entry will be empty
+	const splits = content.split('Author:');
+	const map = new Map<string, GitFile>();
+	splits.forEach(s => parseLogEntry(s, map));
+
+	return map;
+}
